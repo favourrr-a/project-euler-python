68c9a360e00e33eae3bd6623d58544f617ab95d8
nerdfavour <favourackonu13@gmail.com>
2024-09-24 20:59:56 +0000

    problem #005

---
+++ smallest-divisible-num.py
@@ -0,0 +1,4 @@
+for i in range(1, 999999999):
+    if i % 20 == 0 and i % 19 == 0 and i % 18 == 0 and i % 17 == 0 and i % 16 == 0 and i % 15 == 0 and i % 14 == 0 and i % 13 == 0 and i % 12 == 0 and i % 11 == 0:
+        print(i)
+        break
